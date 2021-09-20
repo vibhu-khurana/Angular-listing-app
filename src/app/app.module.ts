535082026a7a343ba6ab6498f69636b3ea6347ba
Vibhu Khurana <kkhurana.vibhu@gmail.com>
2021-09-20 20:52:08 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { RecipesComponent } from './features/recipes/recipes.component';
+import { RecipeListComponent } from './features/recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './features/recipes/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './features/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './features/shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeItemComponent } from './features/recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
